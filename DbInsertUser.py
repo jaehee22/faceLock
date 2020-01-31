d29b8405a68f12ba1f382f9636d4313db6e0f66c
jaeheelee <58822916+jaehee22@users.noreply.github.com>
2020-01-31 19:00:42 +0900

    Create DbInsertUser.py

---
+++ DbInsertUser.py
@@ -0,0 +1,27 @@
+import os
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+cred = credentials.Certificate('./faceKey.json')
+firebase_admin.initialize_app(cred, {'databaseURL' : "https://facelock-39a53.firebaseio.com"})
+
+face_id = 0
+
+for i in range(1,10):
+	n = str(i)
+        if os.path.exists("./dataset/User."+n+".30.jpg") == True:
+                i+=1
+        else:
+                face_id = i
+                break
+
+
+k = str(face_id)
+
+ref = db.reference('User/'+k)
+ref.update({
+        'name' : k
+})
+
+print k
