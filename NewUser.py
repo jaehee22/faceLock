ddce93495fb82f640b799a2ba54ec71a193c966c
jaeheelee <58822916+jaehee22@users.noreply.github.com>
2020-01-31 19:02:44 +0900

    Create NewUser.py

---
+++ NewUser.py
@@ -0,0 +1,117 @@
+import cv2
+import os
+import firebase_admin
+import numpy as np
+from PIL import Image
+import pyrebase
+import time
+from datetime import datetime
+import json
+import os
+from firebase_admin import credentials
+from firebase_admin import db
+
+cred = credentials.Certificate('./faceKey.json')
+firebase_admin.initialize_app(cred, {'databaseURL' : "https://facelock-39a53.firebaseio.com"})
+
+face_id = 0
+
+cap = cv2.VideoCapture(-1)
+cap.set(3,640)
+cap.set(4,480)
+face_detector = cv2.CascadeClassifier('haarcascade/haarcascade_frontalface_default.xml')
+
+
+for i in range(1,10):
+	n = str(i)
+	if os.path.exists("./dataset/User."+n+".30.jpg") == True:
+		i+=1
+	else:
+		face_id = i
+		break
+k = str(face_id)
+
+ref = db.reference('User/'+k)
+ref.update({
+        'name' : k
+})
+
+
+print(" [INFO] Initializing fzce capture. Look the camera and wait ..")
+
+count = 0
+while True:
+	ret, img = cap.read()
+	img = cv2.flip(img, -1)
+	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	faces = face_detector.detectMultiScale(gray, 1.3, 5)
+	for (x,y,w,h) in faces:
+		cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+		count += 1
+		roi_gray = gray[y:y+h, x:x+w]
+		roi_color = img[y:y+h, x:x+w]
+
+		cv2.imwrite("dataset/User." + str(face_id) + '.' +str(count) + ".jpg", gray[y:y+h,x:x+w])
+		cv2.imshow('image',img)
+
+	k = cv2.waitKey(100) & 0xff
+	if k == 27:
+		break
+	elif count >= 30:
+		break
+
+print(" [INFO] Exiting Program and cleanup stuff")
+
+cap.release()
+cv2.destroyAllWindows()
+
+
+#path for face image database
+path = 'dataset'
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+detector = cv2.CascadeClassifier("haarcascade/haarcascade_frontalface_default.xml")
+
+#function to get the image and label data
+def getImagesAndLabels(path):
+        imagePaths = [os.path.join(path,f) for f in os.listdir(path)]
+        faceSamples=[]
+        ids = []
+        for imagePath in imagePaths:
+                PIL_img = Image.open(imagePath).convert('L')
+                img_numpy = np.array(PIL_img,'uint8')
+                id = int(os.path.split(imagePath)[-1].split(".")[1])
+                faces = detector.detectMultiScale(img_numpy)
+                for (x,y,w,h) in faces:
+                        faceSamples.append(img_numpy[y:y+h,x:x+w])
+                        ids.append(id)
+        return faceSamples,ids
+print (" [INFO] Training faces. It will take a few seconds. Wait ...")
+faces,ids = getImagesAndLabels(path)
+recognizer.train(faces, np.array(ids))
+# Save the model into trainer/trainer.yml
+recognizer.write('trainer/trainer.yml')
+# Print the number of faces trained and end program
+print(" [INFO] {0} faces trained. Exiting program".format(len(np.unique(ids))))
+
+
+config ={
+        "apiKey":"faceKey.json",
+        "authDomain":"facelock-39a53.firebaseapp.com",
+        "databaseURL":"http://facelock-39a53.firebaseio.com/",
+        "storageBucket":"facelock-39a53.appspot.com"
+}
+
+ID = str(face_id)
+firebase = pyrebase.initialize_app(config)
+uploadfile = "dataset/User."+ID+".15.jpg"
+s = os.path.splitext(uploadfile)[1]
+filename = ID+".jpg"
+
+storage = firebase.storage()
+
+storage.child("UserList/"+filename).put(uploadfile)
+fileUrl = storage.child("UserList/"+filename).get_url(1)
+print (fileUrl)
+
+
+
